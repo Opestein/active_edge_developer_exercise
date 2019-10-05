942bca70fa8eb160dcd5a016347a25fbd144fa5b
Opeyemi Folarin <opeyemi@intellchub.com>
2019-10-05 13:11:47 +0100

    project setup

---
+++ android/app/src/main/kotlin/com/active_edge_developer_exercise/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.active_edge_developer_exercise
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
